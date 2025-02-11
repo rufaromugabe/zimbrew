d584f1450f9634966a266de3ea0bbae5d339fa8f
rufaro <rufarorevmugabe@gmail.com>
2025-02-11 17:17:33 +0200

    chatbot added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,42 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from "next"
+import localFont from "next/font/local"
+import "./globals.css"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-});
+})
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-});
+})
 
 export const metadata: Metadata = {
   title: "Zim Brew",
   description: "Get your favorite coffee at Zim Brew",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <head>
-        {/* Add favicon */}
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <script
+          data-embed-id="eaa63ade-d1f7-4d50-ac90-6b5310c8e495"
+          data-base-api-url="https://chatai.afrainity.com/api/embed"
+          src="https://chatai.afrainity.com/embed/anythingllm-chat-widget.min.js"
+        ></script>
       </body>
     </html>
-  );
+  )
 }
+
